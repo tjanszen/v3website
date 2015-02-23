380d6656f90386e2fcc1a4c311a9271577d8260c
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-22 18:28:52 -0800

    Add hover jqury functionality

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var paths = {
   jadesrc: ['./client/**/*.jade'],
   lesssrc: ['./client/**/*.less'],
   codesrc: ['./client/**/*.js', './server/**/*.js'],
-  clientsrc: ['./client/**/*.js', './client/**/*.mp3', './client/**/*.jpg', './client/**/*.wav', './client/**/*.png'],
+  clientsrc: ['./client/**/*.js', './client/**/*.mp3', './client/**/*.jpg', './client/**/*.wav', './client/**/*.png', './client/**/*.svg'],
   jadedst: './public',
   lessdst: './public',
   clientdst: './public'

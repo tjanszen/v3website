24d98031fc22f17998a6878d591812141c4f3c46
Tommy Janszen <tommy.janszen@gmail.com>
2015-03-02 17:05:17 -0800

    Add backgrounds, fonts, and update experience ing

--- client/main.js
+++ client/main.js
@@ -2,14 +2,6 @@
 
 $(document).ready(function() {
 
-  $('.portfolio-window').mouseenter(function() {
-    var id = $(this).attr('id');
-    $('.portfolio-window-back-' + id).css("top", "100px")
-  }).mouseleave(function() {
-    var id = $(this).attr('id');
-    $('.portfolio-window-back-' + id).css("top", "300px")
-  });
-
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
